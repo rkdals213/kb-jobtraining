d1176f1b9b953a82739c57c3d9a6279625d1b9be
rkdals213 <rkdals213@naver.com>
2023-03-25 13:02:13 +0900

    asdasd

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,16 +1,12 @@
 <script></script>
 
-<!--<svelte:head>-->
-<!--	<title>Home</title>-->
-<!--	<meta name="description" content="Svelte demo app" />-->
-<!--</svelte:head>-->
-
 <section>
-  <h1>
-    this is home
-  </h1>
+  <div class="button-container">
+    <button class="btn btn-nothing btn-layer-1_2" on:click="{() => {window.open('https://deeply-battery-856.notion.site/46fd6d1ce7c545e28d3dbea40c877c81?v=69460f9b60f442788137d5da97c35bee')}}"> 문제등록</button>
+    <button class="btn btn-nothing btn-layer-1_2" on:click="{() => {window.open('https://github.com/rkdals213/kb-jobtraining/issues')}}"> 이슈등록</button>
+  </div>
 </section>
 
-<style>
+<style lang="scss">
 
 </style>
